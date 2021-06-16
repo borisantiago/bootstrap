66efc3fe5ce65cbf71792d0946f3644b16c2a5e5
BSant <boris96bs7@gmail.com>
2021-06-16 15:11:35 -0500

    final

---
+++ js/funciones.js
@@ -0,0 +1,53 @@
+function btn_formulario(){
+    $("#formulario").show("fast");
+}
+
+function btn_home(){
+    $("#formulario").hide("fast");
+}
+
+function insertar(){
+    if(document.formulario.email.value==""){
+        $("#error_email").show("fast");
+        document.formulario.email.style.border="1px solid red";
+    }
+
+    if(document.formulario.cantidad.value=="seleccione la cantidad"){
+        $("#error_objetos").show("fast");
+        document.formulario.cantidad.style.border="1px solid red";
+    }
+
+    if(document.formulario.area.value==""){
+        $("#error_area").show("fast");
+        document.formulario.area.style.border="1px solid red";
+    }
+
+}
+
+
+//escribiendo nombre
+function colocar_correo(){
+    var email=document.formulario.email.value;
+    expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (expr.test(email) ){
+        document.formulario.email.style.border="1px solid green";
+        $("#error_email").hide("slow");
+        return true;
+    }
+    else{
+        document.formulario.email.style.border="1px solid red";
+        $("#error_email").show("slow");
+        return false;
+    }           
+
+  }
+
+function colocar_objeto(){
+    document.formulario.cantidad.style.border="1px solid green";
+    $("#error_objetos").hide("slow");
+  }
+
+function colocar_area(){
+    document.formulario.area.style.border="1px solid green";
+    $("#error_area").hide("slow");
+  }
